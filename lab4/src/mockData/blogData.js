65575d023d7708431f605ed474fabe97264dfe7c
eJwtzMtqwzAQQNF/mbUWUVoplnZtoZSQpNCUQldiLE1SYdkSIzsPQv69ELy9cO4N0Huq9Tt3NIAFZbxsG0ULucCl14HQ60Y9BUMHXCnVKqOpCWoJAgrnUwzEM73NJzfOq+Nfdvv3j81O/+LPm7ysX7mXn4a/pq0+rV82e35upFlVEPAgbrwWAgstIRODgOrzI2Do42DL1KboXUdXwYTBZj4KppLFVIkt9RiTOGfuDimfQQBdSmSqDkeww5TS/f4PKJBPhg== <nikita.tlewasov@yandex.ru>
2024-10-29 16:43:55 +0500

    Заменил start на flex-start в index.css 4лр

--- lab4/src/mockData/blogData.js
+++ lab4/src/mockData/blogData.js
@@ -1,35 +1,35 @@
 export const blogData = [
     {
       date: "Сент 26, 2021",
-      title: "GPT-3 и OpenAI: это будущее!",
+      title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
       imgSrc: "./assets/img/ai-photo/voronka.png",
       link: "#",
       isLarge: true
     },
     {
       date: "Сент 26, 2021",
-      title: "Как GPT-3 меняет наш мир.",
+      title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
       imgSrc: "../assets/img/ai-photo/face.png",
       link: "#",
       isLarge: false
     },
     {
-      date: "Сент 27, 2021",
-      title: "Будущее технологий: GPT-3.",
+      date: "Сент 26, 2021",
+      title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
       imgSrc: "../assets/img/ai-photo/table.png",
       link: "#",
       isLarge: false
     },
     {
-      date: "Сент 28, 2021",
-      title: "Инновации в искусственном интеллекте.",
+      date: "Сент 26, 2021",
+      title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
       imgSrc: "../assets/img/ai-photo/chain.png",
       link: "#",
       isLarge: false
     },
     {
-      date: "Сент 29, 2021",
-      title: "Этика искусственного интеллекта.",
+      date: "Сент 26, 2021",
+      title: "GPT-3 и Openai – это будущее. Давайте разберемся, как это?",
       imgSrc: "../assets/img/ai-photo/danger.png",
       link: "#",
       isLarge: false
